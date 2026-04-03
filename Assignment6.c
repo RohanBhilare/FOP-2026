a2a5b6a40378bbbf634441adce91d8c5be9a9d33
RohanBhilare <rohanbhilare12@gmail.com>
2026-04-03 11:59:52 +0530

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+long long recursive(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * recursive(n - 1);
+    }
+}
+
+int main() {
+    int n, i;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.");
+    } else {
+        for (i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        printf("Factorial (non-recursive) of %d = %llu\n", n, fact);
+        printf("Factorial (recursive) of %d = %llu\n", n, recursive(n));
+    }
+
+    return 0;
+}
