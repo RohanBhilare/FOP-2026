9a00670b0159800cf424594947b57bbccbbd1b8b
RohanBhilare <rohanbhilare12@gmail.com>
2026-04-03 12:00:18 +0530

    Update Assignment7.c

--- Assignment7.c
+++ Assignment7.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    float marks1, marks2, marks3, avg;
+    int roll_no, prn, i;
+    char student_name[50];
+
+    for (i = 1; i <= 3; i++) {
+        printf("\n--- Student %d ---\n", i);
+
+        printf("Enter student name: ");
+        scanf("%s", student_name);
+
+        printf("Enter roll number: ");
+        scanf("%d", &roll_no);
+
+        printf("Enter PRN of the student: ");
+        scanf("%d", &prn);
+
+        printf("Enter marks of subject 1: ");
+        scanf("%f", &marks1);
+
+        printf("Enter marks of subject 2: ");
+        scanf("%f", &marks2);
+
+        printf("Enter marks of subject 3: ");
+        scanf("%f", &marks3);
+
+        avg = (marks1 + marks2 + marks3) / 3;
+        printf("Average marks of %s: %.2f\n", student_name, avg);
+    }
+
+    return 0;
+}
